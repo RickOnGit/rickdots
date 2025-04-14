30fcbb3075bcb774b04db2166909da10293ead8c
rick <riccardoguadagnini7@gmail.com>
2025-04-14 22:21:44 +0200

    adding zsh stuff

---
+++ programs.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/zsh-users/zsh-completions.git .oh-my-zsh/plugins/
+git clone https://github.com/zsh-users/zsh-autosuggestions.git .oh/oh-my-zsh/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .oh-my-zsh/plugins/
